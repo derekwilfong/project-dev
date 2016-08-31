e14db9623adcd77476629a69aaa411fe39929fc7
derekwilfong <dkwilfong@gmail.com>
2016-08-31 09:16:32 -0700

    Cmd_FindStuff
    Basic filter to find and report information from model.

---
+++ FindStuff/FindStuff/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace FindStuff
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class CmdReportItem : IExternalCommand
+    {
+       
+        public Result Execute(ExternalCommandData commandData, 
+            ref string message, 
+            ElementSet elements)
+        {
+            UIApplication app = commandData.Application;
+            Document doc = app.ActiveUIDocument.Document;
+
+
+            FilteredElementCollector coll = new FilteredElementCollector(doc);
+            IList<Element> doorList = 
+            coll.OfClass(typeof(FamilyInstance))
+            .OfCategory(BuiltInCategory.OST_Doors).ToList();
+            int doorCount = doorList.Count;
+
+            TaskDialog.Show(
+                "Door Count", "Your project has " + doorCount + "doors");
+
+            return Result.Succeeded;
+        }
+    }
+}
