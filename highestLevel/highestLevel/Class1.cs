e26dcc602e2b735d1a2e44e3f0b74fd733b5bf93
Derek Wilfong <dkwilfong@gmail.com>
2016-09-16 08:57:49 -0700

    Add files via upload
    Filtered Element Collector & Parameter Filter to find Level heights. Need to work on LINQ to get highest value

---
+++ highestLevel/highestLevel/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace highestLevel
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class CmdhighestLevel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+
+            UIApplication app = commandData.Application;
+            Document doc = app.ActiveUIDocument.Document;
+
+
+            ElementCategoryFilter lvl = new ElementCategoryFilter(BuiltInCategory.OST_Levels);
+            FilteredElementCollector coll = new FilteredElementCollector(doc);
+            IList<Element> pLvl = coll.WherePasses(lvl).ToElements();
+            foreach (Element e in pLvl)
+            {
+                Parameter paramLvl = e.get_Parameter(BuiltInParameter.LEVEL_ELEV);
+
+                if(paramLvl == null)
+                {
+                    continue;
+                }
+
+
+                string a = string.Format(paramLvl.AsValueString());
+
+
+                TaskDialog.Show(
+                    "Levels",
+                    a, TaskDialogCommonButtons.Ok);
+
+
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
