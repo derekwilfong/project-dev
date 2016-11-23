7af5cfa18bde3aab595818d4206195efbc889395
Derek Wilfong <dkwilfong@gmail.com>
2016-11-23 15:04:41 -0800

    Accessing the Document from other classes.
    Hit a wall. How do you access the 'Document doc' without ExternalCommandData?

---
+++ CreateSheetOop4/CreateSheetOop4/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CreateSheetOop4
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class CmdCreateSheetOop4 : IExternalCommand
+    {
+
+
+        //UIApplication app = commandData.Application;
+        //Document doc = app.ActiveUIDocument.Document;
+        public Document doc;
+        
+
+        Result IExternalCommand.Execute(ExternalCommandData commandData,
+            ref string message, ElementSet elements)
+        {
+        
+
+            doc = commandData.Application.ActiveUIDocument.Document;
+
+            Result newSheet = CreateSheet();
+            return newSheet;
+ 
+            //throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+        public Result CreateSheet()
+        {
+            Titleblock symbolId = new Titleblock();
+            symbolId.TitleblockId();
+
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("CreateSheetOop4");
+                try
+                {
+                    ViewSheet viewSheet = ViewSheet.Create(doc, symbolId.TitleblockId());
+
+                    tx.Commit();
+                }
+                catch (Autodesk.Revit.Exceptions.ApplicationException)
+                {
+                    return Result.Cancelled;
+                }
+            }
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ CreateSheetOop4/CreateSheetOop4/Titleblock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CreateSheetOop4
+{
+    class Titleblock
+    {
+
+        //field data
+
+        
+
+        Document doc;
+        
+
+        public ElementId TitleblockId()
+        {
+            IEnumerable<FamilySymbol> familyList =
+                from elem in new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                let type = elem as FamilySymbol
+                where type.Name.Contains("30")
+                select type;
+
+            ElementId gh = familyList.First().Id;
+            return gh;
+        }
+    }
+
+    
+}
